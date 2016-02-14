8357e460a939cbdf3a20963aff771bc30f8b76d4
Garrett Shorr <g@garrettshorr.com>
2016-02-13 18:17:32 -0800

    First Commit

---
+++ app/src/main/java/com/garrettshorr/warofwords/Word.java
@@ -0,0 +1,98 @@
+package com.garrettshorr.warofwords;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by gshorr on 2/12/16.
+ */
+public class Word implements Parcelable {
+    private int count;
+    private double percentage;
+    private int total;
+    private int raw_count;
+    private String day;
+
+    public Word(int count, double percentage, int total) {
+        this.count = count;
+        this.percentage = percentage;
+        this.total = total;
+    }
+
+    public Word(Parcel p)
+    {
+        count = p.readInt();
+        percentage = p.readDouble();
+        day = p.readString();
+        total = p.readInt();
+        raw_count = p.readInt();
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getRaw_count() {
+        return raw_count;
+    }
+
+    public void setRaw_count(int raw_count) {
+        this.raw_count = raw_count;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String toString() {
+        return "count: " + count + "\npercentage: " + percentage;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(count);
+        dest.writeDouble(percentage);
+        dest.writeString(day);
+        dest.writeInt(total);
+        dest.writeInt(raw_count);
+    }
+
+    public static final Parcelable.Creator<Word> CREATOR = new Parcelable.Creator<Word>() {
+        public Word createFromParcel(Parcel in) {
+            return new Word(in);
+        }
+
+        public Word[] newArray(int size) {
+            return new Word[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/garrettshorr/warofwords/WordCountFragment.java
@@ -0,0 +1,170 @@
+package com.garrettshorr.warofwords;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by g on 2/13/2016.
+ */
+public class WordCountFragment extends Fragment {
+    private TextView mResult;
+    private EditText mSearch;
+    private Button mSearchButton;
+    private Word mWord;
+    private String mUrl;
+
+    public static final String CAP_WORDS_URL = "http://capitolwords.org/api/1";
+    public static final String CLINTON_ID = "C001041";
+    public static final String SANDERS_ID = "S000033";
+
+    private static final String CAP_WORDS_APIKEY = "&apikey=26b803c45ba14dfb94a63bad60bdb21a";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_word_count, container, false);
+
+
+
+
+        mResult = (TextView) rootView.findViewById(R.id.search_result);
+        mSearch = (EditText) rootView.findViewById(R.id.search_term);
+        mSearchButton = (Button) rootView.findViewById(R.id.search_button);
+        mWord = new Word(0,0,0);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search_sunlight_api(mSearch.getText().toString());
+            }
+        });
+        return rootView;
+
+
+    }
+
+    //dates.json?phrase=united+states&entity_type=state&entity_value=VA&apikey=<YOUR_KEY>
+
+
+    private void search_sunlight_api(String text) {
+
+        mUrl = CAP_WORDS_URL + "/dates.json?total=true&percentages=true&phrase=" + mSearch.getText().toString().trim() +
+                "&bioguide_id=" + SANDERS_ID + CAP_WORDS_APIKEY;
+        WordFetcher fetcher = new WordFetcher();
+        fetcher.execute();
+
+    }
+
+    private void handleWord(List<Word> words) {
+        int count = 0;
+        int total = 0;
+        for (Word w : words) {
+            count += w.getCount();
+            total += w.getTotal();
+        }
+        mWord.setCount(count);
+        mWord.setTotal(total);
+        mWord.setPercentage((double) count / total);
+        Log.e("MAIN:", "" + mWord.getCount() + " " + mWord.getTotal() + " " + mWord.getPercentage());
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mResult.setText(mWord.toString());
+            }
+        });
+    }
+
+    private void handleWordFailure() {
+
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mResult.setText("Something went wrong.");
+            }
+        });
+    }
+
+    private void handleWordFailure2() {
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mResult.setText("Something else went wrong.");
+            }
+        });
+    }
+
+    //private inner class for AsyncTask to access the external API
+    private class WordFetcher extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            {
+
+                URL url = null;
+                try {
+                    url = new URL(mUrl);
+//                  url = new URL("http://capitolwords.org/api/1/dates.json?phrase=united+states&entity_type=state&entity_value=VA&apikey=26b803c45ba14dfb94a63bad60bdb21a");
+                    Reader reader = new InputStreamReader(url.openStream());
+                    Scanner s = new Scanner(reader);
+                    String json = "";
+                    while (s.hasNextLine()) {
+                        json += s.nextLine();
+                    }
+
+                    JSONObject jsonObject = new JSONObject(json);
+                    JSONArray internalObject = (JSONArray) jsonObject.get("results");
+
+                    Gson gson = new GsonBuilder().create();
+                    List<Word> words = Arrays.asList(gson.fromJson(internalObject.toString(), Word[].class));
+                    handleWord(words);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    handleWordFailure();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    handleWordFailure2();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        }
+
+    }
+}
